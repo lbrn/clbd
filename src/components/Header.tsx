11668c7e7163f04f4dafeb9b896924100ad54e84
Joshua <joshuatanderson1@gmail.com>
2019-10-22 23:44:50 -0500

    fixing app bar padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Typography, Grid, Toolbar, makeStyles, Container } from '@material-ui/core';
+import { AppBar, Typography, Grid, Toolbar, makeStyles, Container, Button } from '@material-ui/core';
 import logoFull from '../assets/logo_full.png'
 
 import { theme } from '../theme/theme';
@@ -8,12 +8,18 @@ const useStyles = makeStyles({
 	image: {
 		objectFit: 'contain',
 		maxHeight: '6vh',
-		marginTop: '2vh',
-		marginBottom: '2vh',
 	},
 	appBar: {
 		backgroundColor: theme.palette.common.white,
-	}
+		paddingTop: '2vh',
+		paddingBottom: '2vh',
+	},
+	buttonCont: {
+		display: 'flex',
+		alignContent: 'center',
+		justifyContent: 'space-between',
+	},
+
 
 })
 
@@ -22,9 +28,26 @@ const Header = () => {
 	return (
 		<AppBar position="sticky" className={classes.appBar} >
 			<Toolbar>
-				<Grid container justify="flex-start" spacing={3}>
+				<Grid container justify="flex-start" alignContent="center" spacing={3}>
 					<Grid item xs={6}>
 						<img className={classes.image} src={logoFull} />
+					</Grid>
+					<Grid className={classes.buttonCont} container item spacing={1} xs={12} md={6}>
+						<Grid item xs md>
+							<Button color="primary">about</Button>
+						</Grid>
+						<Grid item xs >
+							<Button color="primary">people</Button>
+						</Grid>
+						<Grid item xs>
+							<Button color="primary">research</Button>
+						</Grid>
+						<Grid item xs>
+							<Button color="primary">resources</Button>
+						</Grid>
+						<Grid item xs>
+							<Button color="primary">events</Button>
+						</Grid>
 					</Grid>
 				</Grid>
 			</Toolbar>
