d5d6781660a91f688b886a382ae11c42384eda7c
Joshua <joshuatanderson1@gmail.com>
2019-10-23 15:31:12 -0500

    creating data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Typography, makeStyles, Grid } from '@material-ui/core';
+import { Typography, makeStyles, Grid, Container } from '@material-ui/core';
 
 import { theme } from '../theme/theme';
 
 const useStyles = makeStyles({
 	container: {
+		paddingTop: theme.spacing(3),
 		backgroundColor: theme.palette.common.white,
 		color: theme.palette.primary.main,
 	},
-	// smallFooter: {
-	// 	fontSize: 8
-	// }
 })
 
 const Footer = () => {
 	const classes = useStyles();
 
 	return (
-		<Grid container spacing={3} className={classes.container}>
-			<Grid item xs>
-				<Typography className="smallFooter" variant="body2" align="center">
-					Acknowledgements: The Center for Lung Biology and Disease is supported by the National Institute of General Medical Sciences of the National Institutes of Health under Award Number P20GM130555.
-					The content of this web and research reported in articles resulting from work conducted under this grant are entirely the responsibility of the authors and does not reflect the
-					views of the National Institute of General Medical Sciences.
+		<Container>
+			<Grid container spacing={3} className={classes.container}>
+				<Grid item xs={12}>
+					<Typography className="smallFooter" variant="body2" align="center">
+						Acknowledgements: The Center for Lung Biology and Disease is supported by the National Institute of General Medical Sciences of the National Institutes of Health under Award Number P20GM130555.
+						The content of this web and research reported in articles resulting from work conducted under this grant are entirely the responsibility of the authors and does not reflect the
+						views of the National Institute of General Medical Sciences.
 				</Typography>
+				</Grid>
 			</Grid>
-		</Grid>
+		</Container>
 	)
 }
 
---
+++ src/data/people.ts
@@ -0,0 +1,22 @@
+export const people = {
+	leadership: [
+		{
+			name: 'Samithamby Jeyaseelan',
+			title: 'William L. Jenkins Professor in the Department of Pathobiological Sciences',
+			role: 'principal investigator',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/jey',
+		},
+		{
+			name: 'Rhonda Cardin',
+			title: 'associate dean for research and advanced studies',
+			role: 'co-investigator',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/rcardin'
+		},
+		{
+			name: 'Whitney Fontenot',
+			title: 'Coordinator of grants and development',
+			role: 'Pathobiological Sciences',
+			link: null
+		}
+	]
+}
