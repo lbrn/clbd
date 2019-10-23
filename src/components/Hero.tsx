f4e2afccc6bd7d9246e757c89c0932d9b751c955
Joshua <joshuatanderson1@gmail.com>
2019-10-22 22:42:33 -0500

    hero css swapped to MUI method

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core'
+import beakerImage from '../assets/beakers.jpg'
 
-import './Hero.css';
-
-// const useStyles = makeStyles({
-// 	background: {
-// 		height: '100vh',
-// 		width: '100%',
-// 		background: `
-// 			url(${beakersImage}), 
-// 			linear-gradient(#006, #555)`,
-// 	}
-// })
+const useStyles = makeStyles({
+	background: {
+		height: '100vh',
+		width: '100%',
+		backgroundImage: `linear-gradient(45deg, rgba(0, 0, 0, .7), rgba(19, 127, 222, .6)), url(${beakerImage})`,
+		backgroundSize: 'cover',
+		filter: 'sepia(25%)'
+	}
+})
 
 const Hero = () => {
-	// const classes = useStyles({})
+	const classes = useStyles({})
 	return (
-		<div className={'background'}>
+		<div className={classes.background}>
 
 		</div>
 	)
