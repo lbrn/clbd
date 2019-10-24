41e4efa10ce2e0a9caabca8066896411a3ec65c2
Joshua <joshuatanderson1@gmail.com>
2019-10-24 09:06:55 -0500

    people data is formatted

--- src/data/people.ts
+++ src/data/people.ts
@@ -18,5 +18,146 @@ export const people = {
 			role: 'Pathobiological Sciences',
 			link: null
 		}
+	],
+	externalAdvisoryCommittee: [
+		{
+			name: 'Michael Fessler',
+			location: 'National Institute of Environmental Health Sciences.',
+			link: 'https://www.niehs.nih.gov//research/atniehs/labs/iidl/pi/host-def/index.cfm',
+		},
+		{
+			name: 'Charles Irvin',
+			location: 'University of Vermont.',
+			link: 'https://www.uvm.edu/directorylisting/cirvin',
+		},
+		{
+			name: 'Bruce Stanton',
+			location: 'Dartmouth College.',
+			link: 'https://geiselmed.dartmouth.edu/faculty/facultydb/view.php/?uid=193',
+		},
+		{
+			name: 'Nicole Baumgarth',
+			location: 'University of California, Davis.',
+			link: 'https://ccm.ucdavis.edu/people/nicole-baumgarth',
+		},
+		{
+			name: 'Juliane Bubeckwardenburg',
+			location: 'Washington University in St.Louis.',
+			link: 'https://profiles.wustl.edu/en/persons/juliane-bubeck-wardenburg',
+		},
+	],
+	internalAdvisoryCommittee: [
+		{
+			name: 'Joel Baines',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/jbaines',
+		},
+		{
+			name: 'Deepak Kaushal',
+			link: 'https://www.txbiomed.org/scientists/deepak-kaushal/',
+		},
+		{
+			name: 'Konstantin Kousoulas',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/vtgusk'
+		},
+		{
+			name: 'Stephania Cormier',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/stephaniacormier'
+		},
+		{
+			name: 'Jay Kolls',
+			link: 'https://medicine.tulane.edu/departments/pulmonary-diseases-critical-care-environmental-medicine-research/faculty/jay-kolls-md',
+		},
+		{
+			name: 'Kevin Reed',
+			link: null
+		},
+	],
+	mentees: [
+		{
+			name: 'Weishan Huang',
+			mentors: [
+				{
+					name: 'Stephanie Cormier',
+					link: 'http://facultypages.vetmed.lsu.edu/faculty/stephaniacormier'
+				},
+				{
+					name: 'Jay Kolls',
+					link: 'https://medicine.tulane.edu/departments/pulmonary-diseases-critical-care-environmental-medicine-research/faculty/jay-kolls-md'
+				}
+			],
+		},
+		{
+			name: 'Yogesh Saini',
+			mentors: [
+				{
+					name: 'Rhonda Cardin',
+					link: 'http://facultypages.vetmed.lsu.edu/faculty/rcardin',
+				},
+				{
+					name: 'Deepak Kaushal',
+					link: 'https://www.txbiomed.org/scientists/deepak-kaushal/',
+				}
+			]
+		},
+		{
+			name: 'Sonika Patial',
+			mentors: [
+				{
+					name: 'Jacqueline Stephens'
+				},
+				{
+					name: 'James McLachlan'
+				}
+			]
+		},
+		{
+			name: 'Alexandra Noel',
+			mentors: [
+				{
+					name: 'Tammy Dugas',
+					link: 'http://facultypages.vetmed.lsu.edu/faculty/tammydugas'
+				},
+				{
+					name: 'Stephania Cormier',
+					link: 'http://facultypages.vetmed.lsu.edu/faculty/stephaniacormier'
+				},
+			]
+		},
+	],
+	investigators: [
+		{
+			name: 'Sonika Patial',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/spatial',
+		},
+		{
+			name: 'Yogesh Saini',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/ysaini',
+		},
+		{
+			name: 'Weishan Huang',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/huang1',
+		}
+		,
+		{
+			name: 'Alexandra Noel',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/anoel',
+		}
+		,
+		{
+			name: 'Masami Yoshimmura',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/fukugawa',
+		},
+		{
+			name: 'Antonieta Guerrero-Plata',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/aguerrp',
+		},
+		{
+			name: 'Olalekan Ogundele',
+			link: 'http://facultypages.vetmed.lsu.edu/faculty/ogundele',
+		},
+		{
+			name: 'Tirumalai Rangasamy',
+			link: null
+		}
 	]
 }
