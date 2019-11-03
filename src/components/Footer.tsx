5ae12f422287e21b1014c5aaadd2992b7d7bbcc2
Joshua <joshuatanderson1@gmail.com>
2019-11-02 20:35:37 -0500

    footer still doesn't work...

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,18 +4,23 @@ import { Typography, makeStyles, Grid, Container, Link } from '@material-ui/core
 import { theme } from '../theme/theme';
 
 const useStyles = makeStyles({
-	container: {
-		paddingTop: theme.spacing(3),
+	innerCont: {
 		backgroundColor: theme.palette.common.white,
+		paddingTop: theme.spacing(3)
 	},
+	outerCont: {
+		marginTop: theme.spacing(3),
+		width: '100%',
+		padding: 0,
+	}
 })
 
 const Footer = () => {
 	const classes = useStyles();
 
 	return (
-		<Container>
-			<Grid container spacing={3} className={classes.container}>
+		<Container className={classes.outerCont}>
+			<Grid container spacing={3} className={classes.innerCont}>
 				<Grid container item xs={6} sm={4}>
 					<Grid item xs>
 						<Link href="http://lsu.edu/accessibility">Accessibility statement</Link>
