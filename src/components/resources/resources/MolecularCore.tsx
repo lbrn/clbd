822cf78a2f8b1ab2bf9f1b398116f4b85764db61
Joshua <joshuatanderson1@gmail.com>
2020-01-27 12:12:16 -0600

    name changes

--- src/components/resources/resources/MolecularCore.tsx
+++ src/components/resources/resources/MolecularCore.tsx
@@ -34,32 +34,42 @@ const MolecularCore = props => {
   return (
     <Fragment>
       <Grid item xs={12}>
-        <Typography variant="h5">Molecular Core</Typography>
+        <Typography variant="h5">Molecular Biology Core</Typography>
       </Grid>
       <Grid item xs={12}>
         {/* {createLinks(props.links)} */}
-        <Typography variant="subtitle2">Core</Typography>
-        <Typography>1. Konstantin Kousoulas, PhD, Project Lead</Typography>
-        <Typography>2. Vladamir Chouljenko, PhD, Co- Investigator</Typography>
-        <Typography> 3. Tirumalai Rangasamy, PhD, Co-Investigator</Typography>
+        <Typography variant="subtitle1">
+          <strong>Core</strong>
+        </Typography>
+        <Typography>&bull; Konstantin Kousoulas, PhD, Project Lead</Typography>
+        <Typography>
+          &bull; Vladamir Chouljenko, PhD, Co- Investigator
+        </Typography>
+        <Typography>
+          {' '}
+          &bull; Tirumalai Rangasamy, PhD, Co-Investigator
+        </Typography>
       </Grid>
       <Grid>
-        <Typography variant="subtitle2">Core Services</Typography>
-        <Typography>
-          1. RNAi vectors and CRISPR/Cas9 for gene silencing and knockout.
+        <Typography variant="subtitle1">
+          <strong>Core Services</strong>
         </Typography>
         <Typography>
-          2.Overexpression vectors for transgene expression.
+          &bull; RNAi vectors and CRISPR/Cas9 for gene silencing and knockout.
         </Typography>
-        <Typography>3. Site-directed mutagenesis.</Typography>
-        <Typography>4. Gene and pathway reporter assays.</Typography>
         <Typography>
-          5. Support for transfection using adenovirus and lentivirus vectors.
+          &bull;Overexpression vectors for transgene expression.
         </Typography>
-        <Typography>6. Real-time PCR gene quantification.</Typography>
-        <Typography>7. DNA microarray and RNAseq analyses</Typography>
+        <Typography>&bull; Site-directed mutagenesis.</Typography>
+        <Typography>&bull; Gene and pathway reporter assays.</Typography>
         <Typography>
-          8. Consultation on molecular biology aspects to the COBRE
+          &bull; Support for transfection using adenovirus and lentivirus
+          vectors.
+        </Typography>
+        <Typography>&bull; Real-time PCR gene quantification.</Typography>
+        <Typography>&bull; DNA microarray and RNAseq analyses</Typography>
+        <Typography>
+          &bull; Consultation on molecular biology aspects to the COBRE
           investigators and develop or adopt new techniques as needed.
         </Typography>
       </Grid>
--- src/components/resources/resources/PulmonaryCore.tsx
+++ src/components/resources/resources/PulmonaryCore.tsx
@@ -35,60 +35,70 @@ const PulmoaryCore = props => {
   return (
     <Fragment>
       <Grid item xs={12}>
-        <Typography variant="h5">Pulmonary Core</Typography>
+        <Typography variant="h5">Pulmonary Immunopathology Core </Typography>
       </Grid>
       <Grid item xs={12}>
-        <Typography variant="subtitle2">Core</Typography>
-        <Typography>1. Tammy Dugas, PhD, Project Lead</Typography>
+        <Typography variant="subtitle1"><strong>Core</strong></Typography>
+        <Typography>&bull; Tammy Dugas, PhD, Project Lead</Typography>
         <Typography>
-          2. Ingeborg Langohr, DVM, PhD, DACVP, Co-Investigator
+          &bull; Ingeborg Langohr, DVM, PhD, DACVP, Co-Investigator
         </Typography>
-        <Typography>3. Rebecca Christofferson, PhD, Co-Investigator</Typography>
+        <Typography>
+          &bull; Rebecca Christofferson, PhD, Co-Investigator
+        </Typography>
       </Grid>
       <Grid item xs={12}>
-        <Typography variant="subtitle2">Core Services</Typography>
+        <Typography variant="subtitle1"><strong>Core Services</strong></Typography>
         <Typography>
-          1. Mouse colony management and breeding strategies for maintaining
+          &bull; Mouse colony management and breeding strategies for maintaining
           colonies.
         </Typography>
-        <Typography>2. Genotyping of mouse tail DNA via PCR.</Typography>
-        <Typography>3. Intranasal/intratracheal infection of mice.</Typography>
-        <Typography>4. Post-infection monitoring of mice.</Typography>
-        <Typography>5. Pulmonary function tests, such as Flexivent</Typography>
-        <Typography>6. Mouse dissections and tissue collections.</Typography>
+        <Typography>&bull; Genotyping of mouse tail DNA via PCR.</Typography>
         <Typography>
-          7. Bronchioalveolar lavage fluid (BAL) collection.
+          &bull; Intranasal/intratracheal infection of mice.
+        </Typography>
+        <Typography>&bull; Post-infection monitoring of mice.</Typography>
+        <Typography>
+          &bull; Pulmonary function tests, such as Flexivent
         </Typography>
         <Typography>
-          8. Intratracheal instillation of reagents, such as LPS, bacteria,
+          &bull; Mouse dissections and tissue collections.
+        </Typography>
+        <Typography>
+          &bull; Bronchioalveolar lavage fluid (BAL) collection.
+        </Typography>
+        <Typography>
+          &bull; Intratracheal instillation of reagents, such as LPS, bacteria,
           antibodies, compounds.
         </Typography>
         <Typography>
-          9. Support for evaluations using confocal microscopy and
+          &bull; Support for evaluations using confocal microscopy and
           interpretation of images obtained by scanning and transmission
           electron microscopy.
         </Typography>
         <Typography>
-          10. Support for experimental design related to pathology sampling
+          &bull; Support for experimental design related to pathology sampling
         </Typography>
         <Typography>
-          11. Tissue processing for paraffin-embedded tissue sections and stains.
+          &bull; Tissue processing for paraffin-embedded tissue sections and
+          stains.
         </Typography>
         <Typography>
-          12. Immunofluorescence and immunohistochemistry tissue staining.
+          &bull; Immunofluorescence and immunohistochemistry tissue staining.
         </Typography>
         <Typography>
-          13. Histological scoring of lungs and extrapulmonary tissues.
+          &bull; Histological scoring of lungs and extrapulmonary tissues.
         </Typography>
-        <Typography> 14. Support for Flow cytometry and sorter. </Typography>
+        <Typography> &bull; Support for Flow cytometry and sorter. </Typography>
         <Typography>
-          15. Support for standard tests of immune function including tests of
-          cell mediated immunity (intracellular cytokine staining utilizing flow
-          cytometry), in vitro assays, cytokine gene expression, or cytokine
-          proteins via ELISA.
+          &bull; Support for standard tests of immune function including tests
+          of cell mediated immunity (intracellular cytokine staining utilizing
+          flow cytometry), in vitro assays, cytokine gene expression, or
+          cytokine proteins via ELISA.
         </Typography>
         <Typography>
-          16. Support for statistical analysis (parametric and non-parametric).
+          &bull; Support for statistical analysis (parametric and
+          non-parametric).
         </Typography>
       </Grid>
     </Fragment>
