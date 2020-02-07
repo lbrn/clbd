ebe3a59b3de0c8900efcdcdc465b549b17643a53
Joshua <joshuatanderson1@gmail.com>
2020-02-07 16:57:26 -0600

    link issue fixed home page

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -1,24 +1,24 @@
-import grantPhoto from "../assets/articles/home.jpg";
-import { Saini } from "../assets/people";
+import grantPhoto from '../assets/articles/home.jpg';
+import { Saini } from '../assets/people';
 
 interface article {
-	title: string;
-	image: string;
-	link: string;
+  title: string;
+  image: string;
+  link: string;
 }
 
 // the first article in this array is automatically featured
 
 export const articles: article[] = [
-	{
-		title: "LSU assistant professor receives $2.7 million",
-		image: Saini,
-		link:
-			"https://www.lsu.edu/mediacenter/news/2019/02/04svm_jeyaseelan_nihcenter.php"
-	},
-	{
-		title: "Economic Development Win",
-		image: grantPhoto,
-		link: "https://www.lsu.edu/vetmed/vet_news/saini_ones_award.php"
-	}
+  {
+    title: 'LSU assistant professor receives $2.7 million',
+    image: Saini,
+    link: 'https://www.lsu.edu/vetmed/vet_news/saini_ones_award.php',
+  },
+  {
+    title: 'Economic Development Win',
+    image: grantPhoto,
+    link:
+      'https://www.lsu.edu/mediacenter/news/2019/02/04svm_jeyaseelan_nihcenter.php',
+  },
 ];
