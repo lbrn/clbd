5a51e4d95f9a6743524cbe60ce7f4f1daa638fd2
Joshua <joshuatanderson1@gmail.com>
2020-02-21 12:34:27 -0600

    white change

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,27 +1,29 @@
 import { createMuiTheme } from '@material-ui/core';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 
-
 export const theme = createMuiTheme({
-	palette: {
-		primary: {
-			main: `rgba(19, 127, 222)`,
-		},
-	},
-	typography: {
-		body1: {
-			fontFamily: "Nunito, Roboto, Helvetica, Arial, sans-serif"
-		},
-		body2: {
-			fontFamily: "Nunito, Roboto, Helvetica, Arial, sans-serif"
-		}
-	}
+  palette: {
+    common: {
+      white: `rgba(255, 255, 255, 1)`,
+    },
+    primary: {
+      main: `rgba(19, 127, 222)`,
+    },
+  },
+  typography: {
+    body1: {
+      fontFamily: 'Nunito, Roboto, Helvetica, Arial, sans-serif',
+    },
+    body2: {
+      fontFamily: 'Nunito, Roboto, Helvetica, Arial, sans-serif',
+    },
+  },
 });
 
 export const themeExtended = {
-	palette: {
-		primary: {
-			hover: fade('rgba(19, 127, 222)', .2)
-		}
-	}
-}
+  palette: {
+    primary: {
+      hover: fade('rgba(19, 127, 222)', 0.2),
+    },
+  },
+};
