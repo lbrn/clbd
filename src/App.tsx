7d6d26e00f565f5a77900116849e1f146e75315a
Joshua <joshuatanderson1@gmail.com>
2020-02-25 21:51:27 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React, { Fragment } from 'react';
 import { CssBaseline, makeStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
+import ReactGA from 'react-ga';
 
 import { theme } from './theme/theme';
 import Header from './components/Header';
 import RouterWrapper from './components/router/RouterWrapper';
 import Footer from './components/Footer';
+import { createHistory, LocationProvider } from '@reach/router';
+
+// to get TS to shut up about the window object
+// const windowObj: any = window;
+
+ReactGA.initialize('UA-158668692-1');
+
+const history = createHistory(window);
+
+history.listen( window => {
+  ReactGA.pageview(window.location.pathname+ window.location.search);
+  console.log('page=>',window.location.pathname);
+});
 
 require('dotenv').config();
 
@@ -25,14 +39,16 @@ const App: React.FC = () => {
   return (
     <Fragment>
       <ThemeProvider theme={theme}>
-        <div className={classes.appWrapper}>
-          <div className={classes.contentWrap}>
-            <CssBaseline />
-            <Header />
-            <RouterWrapper />
+        <LocationProvider history={history}>
+          <div className={classes.appWrapper}>
+            <div className={classes.contentWrap}>
+              <CssBaseline />
+              <Header />
+              <RouterWrapper />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
+        </LocationProvider>
       </ThemeProvider>
     </Fragment>
   );
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="react-scripts" />
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/36198
+declare module "history";
