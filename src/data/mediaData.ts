51b7c4794c5dbba150e136cdde01c938418ca9ba
sbentl4 <andmypointis@gmail.com>
2021-10-26 15:11:10 -0500

    hiding Seahorse videos

--- src/data/mediaData.ts
+++ src/data/mediaData.ts
@@ -4,13 +4,6 @@ export const favicon = require('../assets/favicon_io/favicon-16x16.png');
 export const numbers = [1,2];
 export const posts = [
   {
-    id:'1',
-    title: 'Seahorse Zoom Webinar',
-    desc: 'Seahorse Zoom Webinar took place on 07/07/21, 2:00 pm - 3:00 pm. Louisiana State University (Virtual). Agilent Seahorse XF Technology measures the rates of cellular oxygen consumption and acidification of the medium surrounding cells in a microplate. This allows for analysis of changes in mitochondrial and glycolytic function as well as ATP production rates from these two sources. Metabolism drives cellular fate, phenotype, and function and the Seahorse has led the way in showing these connections for well over a decade.',
-    link: 'https://www.youtube.com/watch?v=pg5RcQqxpXs',
-    img: require('../assets/misc/media1.png'),
-  },
-  {
     id:'2',
     title: 'The Essentials for Funding Success',
     desc: 'During the 2021 Louisiana Lung Conference: Pathogenesis of Infectious and Non-Infectious Pulmonary Diseases September 07, 2021 (Virtual), Speaker on The essentials for funding success: How to write a successful application?, Rachel Zemans, M.D., University of Michigan School of Medicine Associate Professor of Medicine, and Speaker on The essentials for funding success: What reviewers are looking for? Chad Steele, Ph.D., Tulane University School of Medicine Chair and Professor of Microbiology and Immunology',
